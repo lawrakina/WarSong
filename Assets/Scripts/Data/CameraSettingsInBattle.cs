c4b874021897d364ad9471a03ab6d8bb60a8959c
lawrakina <lawrik@pm.me>
2021-03-10 11:37:07 +0300

    Add Range limits to CameraSettingsInBattle

--- Assets/Scripts/Data/CameraSettingsInBattle.cs
+++ Assets/Scripts/Data/CameraSettingsInBattle.cs
@@ -8,9 +8,11 @@ namespace Data
     public class CameraSettingsInBattle : ScriptableObject
     {
         [SerializeField]
+        [Range(min: 1.0f, max: 10.0f)]
         public float maxTimeToStopFollowingInPlayer = 1.0f;
 
         [SerializeField]
+        [Range(min: 1.0f, max: 10.0f)]
         public float maxTimeToLerpInPlayer = 1.0f;
     }
 }
