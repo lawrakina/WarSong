5f9a1ad55d90f15751d3b25b448a6308f6c10405
lawrakina <lawrik@pm.me>
2021-08-30 15:06:54 +0300

    Add LayerManager Editor

---
+++ Assets/Code/Editor/LayersManagerEditor.cs
@@ -0,0 +1,22 @@
+﻿using Code.Extension;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(LayerManager))]
+    public class LayersManagerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var layersScripts = (LayerManager) target;
+            if (GUILayout.Button(@"Check Layers"))
+            {
+                layersScripts.CheckLayers();
+            }
+        }
+    }
+}
