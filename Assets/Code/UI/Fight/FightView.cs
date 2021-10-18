212c8b3690e4ebd9568307234057b2e5348ae6b3
lawrakina <marashchuk@gmail.com>
2021-10-18 14:39:16 +0300

    Add 3 buttons of Abilities to FightController

---
+++ Assets/Code/UI/Fight/FightView.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Code.UI.Fight{
+    public sealed class FightView : UiWindow{
+        [SerializeField]
+        private Button _pauseButton;
+
+        [Header("Player UI")]
+        [SerializeField]
+        private Image _person;
+        [SerializeField]
+        private Text _namePlayer;
+        [SerializeField]
+        private Text _healthCountByPlayer;
+        [SerializeField]
+        private Text _resourceCountByPlayer;
+        [SerializeField]
+        private Image _healthBar;
+        [SerializeField]
+        private Image _resourceBar;
+        [Space]
+        [Header("Controls")]
+        [SerializeField]
+        private List<FightAbilityButton> listOfButtonAbilities;
+        [SerializeField]
+        private UltimateJoystick _joystick;
+
+        private float _playerMaxHp = 0f;
+        private float _playerCurrentHp = 0f;
+        private float _playerMaxResource = 0f;
+        private float _playerCurrentResource = 0f;
+
+        public List<FightAbilityButton> ListOfButtonAbilities => listOfButtonAbilities;
+        public UltimateJoystick Joystick => _joystick;
+
+        public float PlayerCurrentHp{
+            get{ return _playerCurrentHp; }
+            set{
+                _playerMaxHp = value;
+                _healthCountByPlayer.text = $"{_playerCurrentHp}/{_playerMaxHp}";
+                _healthBar.fillAmount = _playerCurrentHp / _playerMaxHp;
+            }
+        }
+        public float PlayerMaxHp{
+            get{ return _playerMaxHp; }
+            set{
+                _playerCurrentHp = value;
+                _healthCountByPlayer.text = $"{_playerCurrentHp}/{_playerMaxHp}";
+                _healthBar.fillAmount = _playerCurrentHp / _playerMaxHp;
+            }
+        }
+        public float PlayerCurrentResource{
+            get{ return _playerCurrentResource; }
+            set{
+                _playerMaxResource = value;
+                _resourceCountByPlayer.text = $"{_playerCurrentResource}/{_playerMaxResource}";
+                _resourceBar.fillAmount = _playerCurrentResource / _playerCurrentHp;
+            }
+        }
+        public float PlayerMaxResource{
+            get{ return _playerMaxResource; }
+            set{
+                _playerCurrentResource = value;
+                _resourceCountByPlayer.text = $"{_playerCurrentResource}/{_playerMaxResource}";
+                _resourceBar.fillAmount = _playerCurrentResource / _playerMaxResource;
+            }
+        }
+    }
+}
